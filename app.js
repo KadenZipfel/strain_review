14273cd0a8c9d2b764b0a860c24c4e556c085954
Kaden Zipfel <h.fan98@hotmail.com>
2018-02-06 23:26:28 +0000

    Add show page and add description to new form

--- app.js
+++ app.js
@@ -10,14 +10,16 @@ app.set("view engine", "ejs");
 //Schema Setup
 var strainSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 var Strain = mongoose.model("Strain", strainSchema);
 
 // Strain.create({
 //   name: "Purple Kush", 
-//   image: "https://buyweedonlinewithbitcoins.com/wp-content/uploads/2016/12/Purple-Kush.jpg"
+//   image: "https://buyweedonlinewithbitcoins.com/wp-content/uploads/2016/12/Purple-Kush.jpg",
+//   description: "A very sedative strain with great pain relief."
 // }, function(err, strain){
 //   if(err){
 //     console.log(err);
@@ -31,22 +33,25 @@ app.get("/", function(req, res){
   res.render("landing");
 });
 
+// Index Route
 app.get("/strains", function(req, res){
   // Get all strains from DB
   Strain.find({}, function(err, allStrains){
     if(err){
       console.log(err);
     } else {
-      res.render("strains", {strains:allStrains});
+      res.render("index", {strains:allStrains});
     }
   });
 });
 
+// Create Route
 app.post("/strains", function(req, res){
   //Get data from form and add to strains array
   var name = req.body.name;
   var image = req.body.image;
-  var newStrain = {name: name, image: image};
+  var desc = req.body.description;
+  var newStrain = {name: name, image: image, description: desc};
   //Create a new strain and save to DB
   Strain.create(newStrain, function(err, newlyCreated){
     if(err){
@@ -58,8 +63,22 @@ app.post("/strains", function(req, res){
   });
 });
 
+// New Route
 app.get("/strains/new", function(req, res){
   res.render("new");
+});
+
+// Show Route
+app.get("/strains/:id", function(req, res){
+  //Find the strain with provided ID
+  Strain.findById(req.params.id, function(err, foundStrain){
+    if(err) {
+      console.log(err);
+    } else {
+      //Render show template with that strain
+      res.render("show", {strain: foundStrain});
+    }
+  });
 });
     
 app.listen(process.env.PORT, process.env.IP, function(){
