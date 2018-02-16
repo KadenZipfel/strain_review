89434b7707fb8f2c77acac8349b3fed4efc9abdb
Kaden Zipfel <h.fan98@hotmail.com>
2018-02-16 05:08:01 +0000

    Add comment destroy route

--- routes/comments.js
+++ routes/comments.js
@@ -67,6 +67,17 @@ router.put("/:comment_id", function(req, res){
   });
 });
 
+// Comment destroy route
+router.delete("/:comment_id", function(req, res){
+  Comment.findByIdAndRemove(req.params.comment_id, function(err){
+    if(err){
+      res.redirect("back");
+    } else {
+      res.redirect("/strains/" + req.params.id);
+    }
+  });
+});
+
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
     return next();
