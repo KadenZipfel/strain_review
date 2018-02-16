cb36713cee243609d21164ac3edb83ec15184543
Kaden Zipfel <h.fan98@hotmail.com>
2018-02-16 04:56:54 +0000

    Add comment update route

--- routes/comments.js
+++ routes/comments.js
@@ -45,6 +45,28 @@ router.post("/", isLoggedIn, function(req, res){
   });
 });
 
+// Comment edit route
+router.get("/:comment_id/edit", function(req, res){
+  Comment.findById(req.params.comment_id, function(err, foundComment){
+    if(err){
+      res.redirect("back");
+    } else {
+      res.render("comments/edit", {strain_id: req.params.id, comment: foundComment});
+    }
+  });
+});
+
+// Comment update route
+router.put("/:comment_id", function(req, res){
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+    if(err){
+      res.redirect("back");
+    } else {
+      res.redirect("/strains/" + req.params.id);
+    }
+  });
+});
+
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
     return next();
